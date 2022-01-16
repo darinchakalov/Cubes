c97eba758d8f010221711bbd5f2984f1deca96ad
darinchakalov <darinchakalov@hotmail.com>
2022-01-16 15:11:36 +0200

    login password validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,18 @@
 const router = require("express").Router();
-const authService = require('../services/authService.js')
+const authService = require("../services/authService.js");
+const jwt = require("jsonwebtoken");
 
 router.get("/login", (req, res) => {
 	res.render("auth/login");
 });
 
-router.post("/login", (req, res) => {
-	console.log(req.body);
-	res.redirect("/login");
+router.post("/login", async (req, res) => {
+	let { username, password } = req.body;
+	try {
+		await authService.login(username, password);
+	} catch (error) {
+		res.send(error.message);
+	}
 });
 
 router.get("/register", (req, res) => {
@@ -16,19 +21,20 @@ router.get("/register", (req, res) => {
 
 router.post("/register", async (req, res) => {
 	let { username, password, repeatPassword } = req.body;
-    if (password !== repeatPassword) {
-        return res.send('Passwords need to match')
-    }
-    let userExists = await authService.ifUserExists(username)
-    if (userExists) {
-        return res.send('User already exists')
-    }
-    try {
-        await authService.register(username, password);
-        res.redirect('/')
-    } catch (error) {
-        res.send(error.message)
-    }
+	if (password !== repeatPassword) {
+		return res.send("Passwords need to match");
+	}
+	let userExists = await authService.ifUserExists(username);
+	if (userExists) {
+		return res.send("User already exists");
+	}
+	try {
+		let user = await authService.register(username, password);
+        
+		res.redirect("/");
+	} catch (error) {
+		res.send(error.message);
+	}
 });
 
 module.exports = router;
--- services/authService.js
+++ services/authService.js
@@ -1,4 +1,5 @@
 const User = require("../models/User.js");
+const bcrypt = require("bcrypt");
 
 function register(username, password) {
 	return User.create({ username, password });
@@ -8,9 +9,22 @@ function ifUserExists(username) {
 	return User.exists({ username: username });
 }
 
+function login(username, password) {
+	return User.findOne({ username }).then((user) => {
+		return Promise.all([bcrypt.compare(password, user.password)], user).then(([isValid, user]) => {
+			if (isValid) {
+				return user;
+			} else {
+				throw { message: "Username or password are invalid" };
+			}
+		});
+	});
+}
+
 const authService = {
 	register,
 	ifUserExists,
+	login,
 };
 
 module.exports = authService;
